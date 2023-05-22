150db31a01128eff852fa3870506417a0acd5f8f
Shashank Deshpande <128305551+ShashankD7@users.noreply.github.com>
2023-05-22 14:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let arr = [], articles = [];
+for (let i=0; i<touristSpots.length; i++) {
+	let newArr = touristSpots.split(" ");
+	if (newArr[0] === "a" || newArr[0] === "an" || newArr[0] === "the") {
+		let str = "";
+		articles[i] = newArr[0];
+		for (let j=1; j<newArr.length; j++) {
+			str += newArr[j];
+		}
+		arr[i] = str;
+	}
+	else {
+		articles[i] = "";
+		arr[i] = touristSpots[i];
+	}
+}
 
+Arrays.sort(arr);
+
+return articles+arr;
